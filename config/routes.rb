1665ab8c8cacf12d830951a0d795b68dc295f4b2
Dena Sawyer <denasawyer1111@gmail.com>
2015-07-16 11:30:22 -0400

    generated statuses controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'status/index'
+
+  get 'status/show'
+
+  get 'status/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
